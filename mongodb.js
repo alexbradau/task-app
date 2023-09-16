f0db4d76e2cb03e55292a4126566adf32adc2c70
Alex Bradau <alexbradau@icloud.com>
2023-09-16 16:28:09 -0400

    now logging operation result

--- mongodb.js
+++ mongodb.js
@@ -1,29 +1,34 @@
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const connectionURL = 'mongodb+srv://alexbradau:d34dp00l@hvar.v9wlupn.mongodb.net/'
-    const databaseName = 'task-manager'
 
-    const client = new MongoClient(connectionURL);
- 
+const connectionURL = 'mongodb+srv://alexbradau:d34dp00l@hvar.v9wlupn.mongodb.net/'
+const databaseName = 'task-manager'
+
+const client = new MongoClient(connectionURL);
+
+async function run() {
+
     try {
-        // Connect to the MongoDB cluster
+
         await client.connect();
- 
+
         const db = client.db(databaseName)
-        db.collection('users').insertOne({
+        const result = await db.collection('users').insertOne({
             name: 'Alex',
             age: '27'
         })
 
-    } catch (e) {
-        console.error(e);
+        console.log(
+            `Document was inserted with the _id: ${result.insertedId}`,
+        );
+
+    } finally {
+
+        await client.close();
+
     }
+
 }
 
-main().catch(console.error);
+run().catch(console.dir);
