f080cde53ddf611ab0b13215cc39df23b69018c6
Alex Bradau <alexbradau@icloud.com>
2023-09-16 16:05:07 -0400

    now writing a user to mongo

--- mongodb.js
+++ mongodb.js
@@ -7,7 +7,7 @@ async function main(){
      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
      */
     const connectionURL = 'mongodb+srv://alexbradau:d34dp00l@hvar.v9wlupn.mongodb.net/'
- 
+    const databaseName = 'task-manager'
 
     const client = new MongoClient(connectionURL);
  
@@ -15,21 +15,15 @@ async function main(){
         // Connect to the MongoDB cluster
         await client.connect();
  
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
+        const db = client.db(databaseName)
+        db.collection('users').insertOne({
+            name: 'Alex',
+            age: '27'
+        })
+
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
 }
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
 
 main().catch(console.error);
