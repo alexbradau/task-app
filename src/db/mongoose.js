27b6f6b85db9d58895064a2fff4bd6db1109854c
Alex Bradau <alexbradau@icloud.com>
2023-09-29 11:33:23 -0400

    added new fields and validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const connectionURL = 'mongodb+srv://alexbradau:d34dp00l@hvar.v9wlupn.mongodb.net/task-manager'
 
@@ -17,28 +18,52 @@ const User = mongoose.model('User', {
                 throw new Error('Age must be a positive number')
             }
         }
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Not a valid email address')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        toLowerCase: true,
+        validate(value){
+            if(value.toLowerCase().includes('password')) throw new Error('Password may not contain \'password\'')
+            if(value.length <= 6) throw new Error('Password must be greater than 6 characters')
+        }
     }
 })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        required: false,
+        default: false
     }
 })
 
 const task = new Task({
     description: 'Make more money',
-    completed: false
 })
 
 task.save()
 
 const me = new User({
     name: 'Alex',
-    age: 29
+    age: 29,
+    email: 'alex@email.com',
+    password: 'thisismypass'
 })
 
 me.save()
