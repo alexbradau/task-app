dea271fd94ea7ebbd6f89f79f34f0f98f6fbb3db
Alex Bradau <alexbradau@icloud.com>
2024-04-21 17:37:52 -0400

    removed get user by id call
    redundant because use should only be able to get their own profile

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -22,18 +22,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) return res.status(404).send("User not found")
-        res.status(200).send(user)
-    } catch (error) {
-        console.log(error)
-        res.status(500).send(error.message)
-    }
-})
-
 router.patch('/users/:id', async (req,res) => {
 
     const updates = Object.keys(req.body)
