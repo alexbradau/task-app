51ccc430f167b04b41204cbeaee6658f6e347703
Alex Bradau <alexbradau@icloud.com>
2024-09-22 19:31:24 -0400

    installed SHARP

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -117,4 +117,17 @@ router.delete('/users/me/avatar', auth, async (req,res) => {
     res.send()
 })
 
+router.get('/users/:id/avatar', async (req,res) => {
+    try{
+        const user = await User.findById({_id: req.params.id})
+        if( !user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    }catch(error){
+        res.status(404).send({error: error.message})
+    }
+})
+
 module.exports = router
